5c6beb0ddae177118bf993a2adbf2c3ce3a9ac25
Hema <hema@hema-Inspiron-5520.(none)>
2014-07-23 18:05:52 +0530

    Merged events and updatesgit status

---
+++ src/com/example/nittapp/EventDb.java
@@ -0,0 +1,241 @@
+package com.example.nittapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class EventDb {
+
+	public static final String KEY_DATE = "_date";
+	public static final String KEY_NAME = "_name";
+	public static final String KEY_TIME = "_time";
+	public static final String KEY_DESC = "_desc";
+	public static final String KEY_VENUE = "_venue";
+	public static final String KEY_ORGANISATIONID = "_orgid";
+	public static final String KEY_EVENTID = "_eventid";
+	public static final String KEY_CLUSTERID = "_clusterid";
+	public static final String KEY_CLUSTERNAME = "_clustername";
+	public static final String KEY_UPDATE = "_update";
+	public static final String KEY_DELETE = "_delete";
+	// public static final String KEY_DELETED = "_deleted";
+
+	// public static final String KEY_CHEMAIL = "_chemail";
+	// public static final String KEY_PIC = "_pic";
+
+	private static final String DATABASE_NAME = "MyEventDb";
+	private static final String DATABASE_TABLE = "Events1";
+	private static final int DATABASE_VERSION = 1;
+
+	private DbHelper ourHelper;
+	private Context ourContext;
+	private static SQLiteDatabase ourDatabase;
+
+	private static class DbHelper extends SQLiteOpenHelper {
+
+		public DbHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			db.execSQL("CREATE TABLE " + DATABASE_TABLE + "(" + KEY_NAME
+					+ " TEXT NOT NULL, " + KEY_DATE + " TEXT, " + KEY_TIME
+					+ " TEXT, " + KEY_VENUE + " TEXT, " + KEY_ORGANISATIONID
+					+ " TEXT, " + KEY_EVENTID + " TEXT, " + KEY_CLUSTERID
+					+ " TEXT, " + KEY_CLUSTERNAME + " TEXT, " + KEY_DELETE
+					+ " TEXT, " + KEY_UPDATE + " TEXT, " + KEY_DESC + " TEXT);");
+			// db.execSQL("create table MyEvents(_event text not null, _date text, _time text, _priority varchar(10));");
+
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+			// TODO Auto-generated method stub
+			ourDatabase.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+			onCreate(ourDatabase);
+
+		}
+
+	}
+
+	public EventDb(Context c) {
+		this.ourContext = c;
+		ourHelper = new DbHelper(ourContext);
+	}
+
+	public EventDb open() throws SQLException {
+		ourDatabase = ourHelper.getWritableDatabase();
+		return this;
+	}
+
+	public void close() {
+		ourHelper.close();
+	}
+
+	public long createEntry(String name, String date, String time, String desc,
+			String venue, String org, String eventid, String clusterid,
+			String clustername, String delete, String update) {
+		// TODO Auto-generated method stub
+
+		ContentValues cv = new ContentValues();
+		cv.put(KEY_NAME, name);
+		cv.put(KEY_DATE, date);
+		cv.put(KEY_TIME, time);
+		cv.put(KEY_DESC, desc);
+		cv.put(KEY_VENUE, venue);
+		cv.put(KEY_ORGANISATIONID, org);
+		cv.put(KEY_EVENTID, eventid);
+		cv.put(KEY_CLUSTERID, clusterid);
+		cv.put(KEY_CLUSTERNAME, clustername);
+		cv.put(KEY_DELETE, delete);
+		cv.put(KEY_UPDATE, update);
+
+		return ourDatabase.insert(DATABASE_TABLE, null, cv);
+
+	}
+
+	// public String getData() {
+	public List getEventDetails(String eventid) {
+		// TODO Auto-generated method stub
+		List docList = new ArrayList();
+		String[] columns = new String[] { KEY_NAME, KEY_DATE, KEY_TIME,
+				KEY_VENUE, KEY_DESC };
+
+		Cursor c = ourDatabase.query(DATABASE_TABLE, columns, "_eventid='"
+				+ eventid + "'", null, null, null, null);
+
+		System.out.println("no of records: " + c.getCount());
+		int iEvent = c.getColumnIndex(KEY_NAME);
+		int iDate = c.getColumnIndex(KEY_DATE);
+		int iTime = c.getColumnIndex(KEY_TIME);
+		int iDesc = c.getColumnIndex(KEY_DESC);
+		int iVenue = c.getColumnIndex(KEY_VENUE);
+
+		for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+			// result = result + c.getString(iEvent) + " " + c.getString(iDate)
+			// + " " + c.getString(iTime) + " " + c.getString(iP) + "\n" ;
+			HashMap temp = new HashMap();
+			temp.put("name", c.getString(iEvent).toUpperCase());
+			temp.put("date", c.getString(iDate));
+			temp.put("time", c.getString(iTime));
+			temp.put("desc", c.getString(iDesc));
+			temp.put("venue", c.getString(iVenue));
+			// temp.put("pic", c.getInt(iPic));
+
+			docList.add(temp);
+
+		}
+		return docList;
+	}
+
+	public void delete(String eventid) {
+		// TODO Auto-generated method stub
+
+		ourDatabase.delete(DATABASE_TABLE, "_eventid='" + eventid + "'", null);
+
+	}
+
+	public List getEvents(String clusterid) {
+		// TODO Auto-generated method stub
+		String[] columns = new String[] { KEY_NAME };
+		Cursor m = ourDatabase.query(DATABASE_TABLE, columns, "_clusterid='"
+				+ clusterid + "'", null, null, null, KEY_NAME + " ASC");
+		List docList = new ArrayList<String>();
+		int iEvent = m.getColumnIndex(KEY_NAME);
+
+		for (m.moveToFirst(); !m.isAfterLast(); m.moveToNext()) {
+
+			docList.add(m.getString(iEvent));
+		}
+
+		return docList;
+	}
+
+	public List getClusters(String orgid) {
+		// TODO Auto-generated method stub
+		String[] columns = new String[] { KEY_CLUSTERNAME };
+		List docList = new ArrayList<String>();
+		Cursor m = ourDatabase.query(DATABASE_TABLE, columns, "_orgid='"
+				+ orgid + "'", null, KEY_CLUSTERNAME, null, KEY_CLUSTERNAME
+				+ " ASC");
+		int iEvent = m.getColumnIndex(KEY_CLUSTERNAME);
+
+		for (m.moveToFirst(); !m.isAfterLast(); m.moveToNext()) {
+
+			docList.add(m.getString(iEvent));
+		}
+
+		return docList;
+	}
+
+	public List getOrgList() {
+		// TODO Auto-generated method stub
+
+		List docList = new ArrayList<String>();
+
+		docList.add("Admin");
+		docList.add("Festember");
+		docList.add("Pragyan");
+
+		return docList;
+	}
+
+	public int getClusterId(String name) {
+		String itemname = null;
+		String[] columns = new String[] { KEY_CLUSTERID };
+		Cursor m = ourDatabase.query(DATABASE_TABLE, columns, "_clustername='"
+				+ name + "'", null, null, null, null);
+		Log.e("Cursor", m.toString());
+		int iEvent = m.getColumnIndex(KEY_CLUSTERID);
+		for (m.moveToFirst(); !m.isAfterLast(); m.moveToNext()) {
+
+		 itemname=m.getString(iEvent);
+		}
+		Log.e(itemname, "Cluster id");
+		return Integer.parseInt(itemname);
+	}
+	
+	public int getEventId(String name) {
+		String itemname = null;
+		String[] columns = new String[] { KEY_EVENTID };
+		Cursor m = ourDatabase.query(DATABASE_TABLE, columns, "_name='"
+				+ name + "'", null, null, null, null);
+		
+		int iEvent = m.getColumnIndex(KEY_EVENTID);
+		for (m.moveToFirst(); !m.isAfterLast(); m.moveToNext()) {
+
+		 itemname=m.getString(iEvent);
+		}
+		
+		return Integer.parseInt(itemname);
+	}
+	
+	public int checkId(String name) {
+		String itemname = null;
+		String[] columns = new String[] { KEY_EVENTID };
+		Cursor m = ourDatabase.query(DATABASE_TABLE, columns, "_eventid='"
+				+ name + "'", null, null, null, null);
+		//Log.e("Cursor", m.toString());
+		int iEvent = m.getColumnIndex(KEY_EVENTID);
+		for (m.moveToFirst(); !m.isAfterLast(); m.moveToNext()) {
+
+		 itemname=m.getString(iEvent);
+		}
+		
+		if(itemname!=null)
+		return Integer.parseInt(itemname);
+		else
+			return 0;
+	}
+
+}
--- src/com/example/nittapp/MainActivity.java
+++ src/com/example/nittapp/MainActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -39,20 +40,39 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	ListView eventsList, updatesList;
+	static int orgid, clusterid;
 	static int lengthOfUpdates=0;
-	String Ename,Edate,Edesc,Evenue,Etime;
-	LoadData updates;
+	static int page_no = 1;
+
+	String Uname,Udate,Udesc,Uvenue,Utime;
+	LoadDataUpdates updates;
+	LoadDataEvents event;
+	ArrayAdapter<String> adapter1;
+	EventDb events;
+	
 	UpdateDB update;
-    
+	List<String> listForEvents;
 	List<HashMap<String, String>> list_of_updates;
 
+	ArrayList<String> uname = new ArrayList<String>();
+	
+	//temp variables
+	ArrayList<String> venue = new ArrayList<String>();
+	ArrayList<String> edesc = new ArrayList<String>();
 	ArrayList<String> ename = new ArrayList<String>();
+	ArrayList<String> edate = new ArrayList<String>();
+	ArrayList<String> etime = new ArrayList<String>();
+	ArrayList<String> evenue = new ArrayList<String>();
+
+	
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		updates = new LoadData();
+		updates = new LoadDataUpdates();
+		event=new LoadDataEvents();
 		TabHost th = (TabHost) findViewById(R.id.tabhost);
 		th.setup();
 		TabSpec specs = th.newTabSpec("tag1");
@@ -65,12 +85,15 @@ public class MainActivity extends Activity {
 		th.addTab(specs);
 
 		update = new UpdateDB(MainActivity.this);
-		
+		events = new EventDb(MainActivity.this);
 		update.open();
 
 		eventsList = (ListView) findViewById(R.id.list1);
 		updatesList = (ListView) findViewById(R.id.list2);
 
+		setList();
+		event.execute("http://10.0.2.2:8080/events");
+		
 		updatesList.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> arg0, View v, int position,
 					long id) {
@@ -85,22 +108,149 @@ public class MainActivity extends Activity {
 		});
 
 		
-		updates.execute("http://10.0.2.2:8080");
+		updates.execute("http://10.0.2.2:8080/updates");
 		
+
+	
+	adapter1 = new MyCustomAdapterUpdate(MainActivity.this, R.layout.row, uname);
+	// updatesList.setAdapter(adapter1);
+
+	eventsList.setOnItemClickListener(new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1,
+				int position, long arg3) {
+			// TODO Auto-generated method stub
+			if (page_no == 1){
+				orgid = position + 1;
+				page_no++;
+			}
+			else if (page_no == 2) {
+				events.open();
+				Log.e("name of clicked position",
+						listForEvents.get(position));
+				clusterid = events.getClusterId(listForEvents.get(position));
+				events.close();
+				page_no++;
+			}
+			else{
+				events.open();
+				Intent intent=new Intent(MainActivity.this, EventDetails.class);
+				intent.putExtra("EventId",events.getEventId(listForEvents.get(position)) );
+				startActivity(intent);
+				events.close();
+			}
+
+			
+			setList();
+
+		}
+	});
+}
+
+	
+	
+
+	private void setList() {
+		// TODO Auto-generated method stub
+		events.open();
+		switch (page_no) {
+		case 1:
+			listForEvents = events.getOrgList();
+			eventsList.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_list_item_1, listForEvents));
+			break;
+
+		case 2:
+			listForEvents = events.getClusters(orgid + "");
+			eventsList.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_list_item_1, listForEvents));
+			break;
+		case 3:
+			listForEvents = events.getEvents(clusterid + "");
+			eventsList.setAdapter(new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_list_item_1, listForEvents));
+			Log.e("Setting events", "yayy");
+			break;
+
+		}
+		events.close();
 
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+			// Things to Do
 
+			page_no--;
+			if (page_no == 0) {
+				page_no = 1;
+				finish();
+			}
+
+			setList();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	//Events
+	
 	public class MyCustomAdapter extends ArrayAdapter<String> {
 		public int cnt = 0;
 
 		public MyCustomAdapter(Context context, int textViewResourceId,
+				ArrayList<String> ename) {
+			super(context, textViewResourceId, ename);
+			Log.d("ADAPT", "Constructing");
+
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// Inflate the layout, mainlvitem.xml, in each row.
+			LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+			View row1 = inflater.inflate(R.layout.row, parent, false);
+			// Log.d("ADAPT", Integer.toString(cnt++));
+
+			TextView item = (TextView) row1.findViewById(R.id.Name);
+			item.setText(ename.get(position));
+			//
+			TextView item1 = (TextView) row1.findViewById(R.id.By);
+			item1.setText(edate.get(position));
+			//
+
+			TextView item3 = (TextView) row1.findViewById(R.id.Venue);
+			item3.setText(evenue.get(position));
+
+			return row1;
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return ename.size();
+		}
+	}
+
+	
+	
+	
+	//Updates
+	public class MyCustomAdapterUpdate extends ArrayAdapter<String> {
+		public int cnt = 0;
+
+		public MyCustomAdapterUpdate(Context context, int textViewResourceId,
 				ArrayList<String> ename) {
 			super(context, textViewResourceId, ename);
 			Log.d("ADAPT", "Constructing");
@@ -137,7 +287,142 @@ public class MainActivity extends Activity {
 		
 	}
 
-	public class LoadData extends AsyncTask<String, Void, String> {
+	
+	public class LoadDataEvents extends AsyncTask<String, Void, String> {
+
+		ArrayList<String> venue = new ArrayList<String>();
+		ArrayList<String> edesc = new ArrayList<String>();
+		ArrayList<String> ename = new ArrayList<String>();
+		ArrayList<String> edate = new ArrayList<String>();
+		ArrayList<String> etime = new ArrayList<String>();
+		ArrayList<String> evenue = new ArrayList<String>();
+
+		private static final String TAG_NAME = "name";
+		private static final String TAG_UPDATE = "update";
+		private static final String TAG_DATE = "date";
+		private static final String TAG_TIME = "time";
+		private static final String TAG_DESC = "desc";
+		private static final String TAG_VENUE = "venue";
+		private static final String TAG_DELETE = "delete";
+		private static final String TAG_ORGANISATIONID = "orgid";
+		private static final String TAG_EVENTID = "eventid";
+		private static final String TAG_CLUSTERID = "clusterid";
+		private static final String TAG_CLUSTERNAME = "clustername";
+
+		/*
+		 * private static final String TAG_LAT = "lat"; private static final
+		 * String TAG_PIC = "pic"; private static final String TAG_LNG = "lng";
+		 * private static final String TAG_EID = "eid"; private static final
+		 * String TAG_UID = "uid";
+		 */
+		JSONArray contacts = null;
+
+		InputStream is = null;
+		JSONObject jObj = null;
+		String json = "";
+
+		protected void onPreExecute(String json) {
+			// TODO Auto-generated method stub
+
+			System.out.println("pre-execute");
+
+		}
+
+		@Override
+		protected String doInBackground(String... urls) {
+			// TODO Auto-generated method stub
+			String url = urls[0];
+			try {
+				// defaultHttpClient
+
+				DefaultHttpClient httpClient = new DefaultHttpClient();
+				HttpGet httpGet = new HttpGet(url);
+
+				HttpResponse httpResponse = httpClient.execute(httpGet);
+				HttpEntity httpEntity = httpResponse.getEntity();
+				is = httpEntity.getContent();
+
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "iso-8859-1"), 8);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+				is.close();
+				json = sb.toString();
+				System.out.println("sb : " + sb);
+			} catch (Exception e) {
+				Log.e("Buffer Error", "Error converting result " + e.toString());
+			}
+
+			return json;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			System.out.println("post-execute");
+			try {
+				events.open();
+				System.out.println("post-execute");
+				jObj = new JSONObject(json);
+				contacts = jObj.getJSONArray("event");
+				int i;
+				for (i = 0; i < contacts.length(); i++) {
+					JSONObject obj = contacts.getJSONObject(i);
+					String Ename = obj.getString(TAG_NAME);
+					String Edate = obj.getString(TAG_DATE);
+					String Etime = obj.getString(TAG_TIME);
+					String Edesc = obj.getString(TAG_DESC);
+					String Evenue = obj.getString(TAG_VENUE);
+					String Eorg = obj.getString(TAG_ORGANISATIONID);
+					String Eclusterid = obj.getString(TAG_CLUSTERID);
+					String Eclustername = obj.getString(TAG_CLUSTERNAME);
+					String Eeventid = obj.getString(TAG_EVENTID);
+					String deleted = obj.getString(TAG_DELETE);
+					String update = obj.getString(TAG_UPDATE);
+
+					if (!deleted.equals("1")) {
+						if (update.equals("1")) {
+							events.delete(Eeventid);
+						}
+						if (events.checkId(Eeventid) == 0) {
+							events.createEntry(Ename, Edate, Etime, Edesc,
+									Evenue, Eorg, Eeventid, Eclusterid,
+									Eclustername, deleted, update);
+
+						}
+					} else
+						events.delete(Eeventid);
+
+				}
+				events.close();
+
+			} catch (JSONException e) {
+				Log.e("JSON Parser", "Error parsing data " + e.toString());
+			}
+
+			setList();
+		}
+
+	}
+
+	
+	
+	
+	
+	public class LoadDataUpdates extends AsyncTask<String, Void, String> {
 
 		private static final String TAG_NAME = "name";
 		private static final String TAG_DATE = "date";
@@ -216,15 +501,15 @@ public class MainActivity extends Activity {
 				int i;
 				for (i = 0; i < contacts.length(); i++) {
 					JSONObject obj = contacts.getJSONObject(i);
-					Ename = obj.getString(TAG_NAME);
-					Edate = obj.getString(TAG_DATE);
-					Etime = obj.getString(TAG_TIME);
-					Edesc = obj.getString(TAG_DESC);
-					Evenue = obj.getString(TAG_VENUE);
+					Uname = obj.getString(TAG_NAME);
+					Udate = obj.getString(TAG_DATE);
+					Utime = obj.getString(TAG_TIME);
+					Udesc = obj.getString(TAG_DESC);
+					Uvenue = obj.getString(TAG_VENUE);
 					
-					ename.add(i, Ename);
+					uname.add(i, Uname);
 					if(i>=lengthOfUpdates){
-					update.createEntry(Ename, Edate, Edesc, Evenue,Etime);}
+					update.createEntry(Uname, Udate, Udesc, Uvenue,Utime);}
 					// Log.e("name", ename.get(i));
 				}
 				lengthOfUpdates = i;
@@ -237,8 +522,8 @@ public class MainActivity extends Activity {
 			list_of_updates = update.getData();
 			update.close();
 			Log.e("length", list_of_updates.size() + "");
-			ArrayAdapter<String> adapter1 = new MyCustomAdapter(
-					MainActivity.this, R.layout.row, ename);
+			ArrayAdapter<String> adapter1 = new MyCustomAdapterUpdate(
+					MainActivity.this, R.layout.row, uname);
 			updatesList.setAdapter(adapter1);
 			//Log.e("hema ", "akhila");
 
